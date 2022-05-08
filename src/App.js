a75e137dbcdc5eb0f4bfef0602ee741b0c1887be
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2022-05-08 21:51:39 +0300

    added tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div data-test="component-app">
-      App
+      <h1 data-test="counter-display">
+        The counter is currently
+        <span data-test="count">{count}</span>
+      </h1>
+      {count < 0 && (
+        <h2 data-test="count-error">
+          The counter cannot go below 0
+        </h2>
+      )}
+      <button
+        data-test="increment-button"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment counter
+      </button>
+      <button
+        data-test="decrement-button"
+        onClick={() => setCount(count - 1)}
+      >
+        Decrement counter
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -6,24 +6,53 @@ Enzyme.configure({
   adapter: new EnzymeAdapter(),
 });
 
+const setUp = () => shallow(<App />);
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
+
 test('render without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']")
+  const wrapper = setUp();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe(1)
 });
 
-test('renders increment button', () => {
-
+test('renders button', () => {
+  const wrapper = setUp();
+  const button = findByTestAttr(wrapper,'increment-button');
+  expect(button.length).toBe(1)
 })
 
 test('renders counters display', () => {
-
+  const wrapper = setUp();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1)
 })
 
 test('counter display starts at 0', () => {
-
+  const wrapper = setUp();
+  const count = findByTestAttr(wrapper, 'count').text();
+  expect(count).toBe("0")
 })
 
 test('clicking button increments counter display', () => {
+  const wrapper = setUp();
+  const button = findByTestAttr(wrapper,'increment-button');
+  button.simulate('click');
+  const count = findByTestAttr(wrapper, 'count').text();
+  expect(count).toBe("1")
+})
 
+test('clicking button decrements counter display', () => {
+  const wrapper = setUp();
+  const button = findByTestAttr(wrapper, 'decrement-button');
+  button.simulate('click');
+  const count= findByTestAttr(wrapper, 'count').text();
+  expect(count).toBe("-1")
+});
+
+test('counter less then 0 show error', () => {
+  const wrapper = setUp();
+  const button = findByTestAttr(wrapper, 'decrement-button')
+  button.simulate('click');
+  const countError = findByTestAttr(wrapper, 'count-error')
+  expect(countError.length).toBe(1);
 })
