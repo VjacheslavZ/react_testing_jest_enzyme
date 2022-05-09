313d8aa2f01bcc04bafb0c7dbde069645436ba9c
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2022-05-09 15:06:23 +0300

    configure tests

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,11 +1,9 @@
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
-
+// import Enzyme, { shallow } from 'enzyme';
+// import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { shallow } from 'enzyme';
 import { findByTestAttr } from "../test/testUtils";
 
 import Congrats from './Congrats';
-
-Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 const setup = (props = {}) => shallow(<Congrats {...props} />)
 
