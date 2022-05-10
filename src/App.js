979fa9929f1c7d40015feeb74fe9bf03ae57b8a2
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2022-05-10 17:26:30 +0300

    add functional tests

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 
 import Congrats from "./Congrats";
 import GuessedWord from "./GuessedWord";
+import Input from "./input";
 
 function App() {
+  const success = false;
+  const secretWord = 'party';
+  const guessedWords = [];
+
   return (
     <div className="container" data-test='component-app'>
       <h1>Jotto</h1>
-      <Congrats success/>
-      <GuessedWord guessedWords={[ { guessedWord: 'train', letterMatchCount: 3 } ]} />
+      <Congrats success={success}/>
+      <Input success={success} secretWord={secretWord} />
+      <GuessedWord guessedWords={guessedWords} />
     </div>
   );
 }
